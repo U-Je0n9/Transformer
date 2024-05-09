05d2b668d6ff41404dfa83365bd03b76be546562
U_Je0n9 <sujsuj1111111@gmail.com>
2024-05-09 20:34:04 +0900

    commit

---
+++ download.py
@@ -0,0 +1,4 @@
+from huggingface_hub import hf_hub_download
+hf_hub_download(repo_id="iwslt2017", filename="iwslt2017.py", repo_type="dataset", revision="data/2017-01-trnted/texts/de/en/de-en.zip")
+
+
